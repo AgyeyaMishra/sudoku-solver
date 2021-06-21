e1522f79a2e38365a4e054bb69647fb675bf0cb4
Agyeya Mishra <53916781+AgyeyaMishra@users.noreply.github.com>
2021-06-22 04:53:49 +0530

    Create Sudoku Solver.cpp

---
+++ Sudoku Solver.cpp
@@ -0,0 +1,115 @@
+//Sudoku Solver in C++ using Backtrackig algorithm
+#include <iostream>
+#include <stdio>
+
+#define UNASSIGNED 0
+
+#define N 9
+
+bool findEmptyPlace(int sudoku[N][N], int& row, int& column);
+
+bool isValid(int sudoku[N][N], int row, int column, int number);
+
+bool solveSudoku(int sudoku[N][N])
+{
+    int row, column;
+    
+    //if there is no unassigned location
+    if(!findEmptyPlace(sudoku, row, column))
+        //success
+        return true;
+    
+    //considering digits 1 to 9
+    for(int number = 1; number <= 9; number++)
+    {
+        //checks if it looks promising
+        if(isValid(sudoku, row, column, number))
+        {
+            //making tentaive assignment
+            sudoku[row][column] = number;
+            
+            //on success, it returns true
+            if(solveSudoku(sudoku))
+                return true;
+            
+            //on failure, it unassigns and we try again
+            sudoku[row][column] = UNASSIGNED;
+        }
+    }
+    
+    //this triggers backtracking algorithm
+    return false;
+}
+
+bool findEmptyPlace(int sudoku[N][N], int& row, int& column)
+{
+    for(row = 0; row < N; row++)
+    {
+        for(column = 0; column < N; column++)
+        {
+            if(sudoku[row][column] == UNASSIGNED)
+                return true;
+        }
+    }
+    
+    return false;
+}
+
+bool isPresentInRow(int sudoku[N][N], int row, int number)
+{
+    for(int column = 0; column < N; column++)
+    {
+        if(sudoku[row][column] == number)
+            return true;
+            
+    }
+    
+    return false;
+}
+
+bool isPresentInColumn(int sudoku[N][N], int column, int number)
+{
+    for(int row = 0; row < N; row++)
+    {
+        if(sudoku[row][column] == number)
+            return true;
+
+    }
+    
+    return false;
+}
+
+bool isPresentInBox(int sudoku[N][N], int boxStartRow, int boxStartColumn, int number)
+{
+    for(int row = 0; row < 3; row++)
+    {
+        for(int column = 0; column < 3; column++)
+        {
+            if(sudoku[row + boxStartRow][column + boxStartColumn] == number)
+                return true;
+                
+        }
+    }
+    
+    return false;
+}
+
+bool isValid(int sudoku[N][N], int row, int column, int number)
+{
+    //checks if number is not already placed in current row, column and current 3X3 box
+    return (!isPresentInRow(sudoku, row, number) && !isPresentInCoumn(sudoku, column, number) && !isPresentInBox(sudoku, row - row % 3, column - column % 3, number) && sudoku[row][column] == UNASSIGNED);
+}
+
+void printSudoku(int sudoku[N][N])
+{
+    cout<"Solved Sudoku is given as:\n";
+    for(int row = 0; row < N; row++)
+    {
+        for(int column = 0; column < N; column++)
+        {
+            cout<<sudoku[row][coumn]<<" ";]
+        }
+        cout<<endl;
+    }
+}
+
